4c0897906125f5a2351a89d27a134e79aa5bd7d0
Chris Oliver <excid3@gmail.com>
2024-11-21 16:19:21 -0600

    Initial commit

---
+++ lib/black_friday.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "black_friday/version"
+
+module BlackFriday
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/black_friday/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BlackFriday
+  VERSION = "0.1.0"
+end
